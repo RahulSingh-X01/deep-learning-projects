da12d263b40e8b77ece921e81e5d4a1b9465141f
Rahul Singh <rahulxsingh2005@gmail.com>
2026-07-25 16:43:13 +0000

    feat: add inference pipeline for cat vs dog classification

---
+++ dog-vs-cat-cnn/inference.py
@@ -0,0 +1,18 @@
+from src.model import load_pretrained_model, create_model
+from src.predict import predict
+from src.utils import load_model, get_device
+from src.data_transformation import transform_data
+from PIL import Image
+
+def load_pipeline():
+    device = get_device()
+    model = load_pretrained_model()
+    model = create_model(model)
+    model = load_model(model)
+    _, transform = transform_data()
+    return model, transform, device
+
+def run_inference(image):
+    model, transform, device = load_pipeline()
+    prediction = predict(model, image, transform, ['Cat', 'Dog'], device)
+    return prediction
